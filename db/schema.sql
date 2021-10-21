2435bfb6f34d354510229b4fe35502c406fe4ad3
ObeeK <chasenharvey@gmail.com>
2021-10-21 12:53:38 -0500

    finished parties

--- db/schema.sql
+++ db/schema.sql
@@ -2,9 +2,9 @@ DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
 
 CREATE TABLE parties (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    description TEXT
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
 );
 
 CREATE TABLE candidates (
@@ -15,4 +15,3 @@ CREATE TABLE candidates (
   industry_connected BOOLEAN NOT NULL,
   CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
 );
-
